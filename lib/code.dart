3cc7e4dae81acba63160eb634072e0d9c7921ca1
Krinal <krinalnakarani1774@gmail.com>
2022-12-27 08:56:57 +0530

    27-12-2022

--- lib/code.dart
+++ lib/code.dart
@@ -20,14 +20,16 @@ class _codeState extends State<code> {
             Padding(
               padding: EdgeInsets.only(top: 30),
                 child: TextField(
+                  cursorColor: Colors.grey.shade400,
                  decoration: InputDecoration(
+                   hintText: "ORIGINAL PRICE",
                    filled: true,
-                   fillColor: Colors.grey.shade100,
+                   fillColor: Colors.grey.shade300,
                    enabledBorder: OutlineInputBorder(
                      borderSide: BorderSide(color: Colors.grey.shade100,width: 2)
                    ),
                    focusedBorder: OutlineInputBorder(
-                     borderSide: BorderSide(color: Colors.grey.shade300,width: 5),
+                     borderSide: BorderSide(color: Colors.grey.shade600,width: 5),
                    )
                  ),
                 )),
@@ -35,7 +37,7 @@ class _codeState extends State<code> {
               margin: EdgeInsets.only(top: 20),
               height: 110,
               width: double.infinity,
-              color: Colors.grey.shade100,
+              color: Colors.grey.shade300,
               child: Column(
                 children: [
                   Center(
@@ -160,8 +162,44 @@ class _codeState extends State<code> {
             ),
             SizedBox(height: 20,),
             Container(
+              height: 60,
+              width: double.infinity,
+              color: Colors.grey.shade300,
+              child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 40),
+                    height: 50,
+                    width: 100,
+                    child: Center(
+                      child: Text(
+                        "Final Price",
+                        style: TextStyle(
+                            fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 250),
+                    height: 50,
+                    width: 110,
+                    child: Center(
+                      child: Text(
+                        "1050.00 Rs",
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20,),
+            Container(
               height: 90,
-              width: 250,
+              width: double.infinity,
               child: Center(child: Text("CGST/SGST\n         25",
               style: TextStyle(
                 fontSize: 25,
@@ -169,30 +207,189 @@ class _codeState extends State<code> {
               ),
               )),
               decoration: BoxDecoration(
-                color: Colors.grey.shade100,
+                color: Colors.grey.shade300,
                 borderRadius: BorderRadius.circular(10)
               ),
             ),
             SizedBox(height: 20,),
-            Row(
-              children: [
-                Container(
-                  margin: EdgeInsets.only(left: 10),
-                  alignment: Alignment.centerLeft,
-                  height: 300,
-                  width: 250,
-                  color: Colors.red,
-                ),
-                SizedBox(width: 15,),
-                Container(
-                  margin: EdgeInsets.only(left: 50,bottom: 70),
-                  width: 80,
-                  height: 225,
-                  color: Colors.red,
-                ),
-              ],
-            )
-
+            Container(
+              height: 479,
+              width: double.infinity,
+              child: Row(
+                children: [
+                  Container(
+                    //margin: EdgeInsets.only(right: 155),
+                    height: 479,
+                    width: 400,
+                   // alignment: Alignment.bottomCenter,
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 100,
+                          width: 400,
+                          child: RichText(
+                            text: TextSpan(
+                                children: [
+                                  TextSpan(
+                                      text: "  7          ",
+                                      style: TextStyle(
+                                          fontSize: 50,
+                                          color: Colors.black
+                                      )
+                                  ),
+                                  TextSpan(
+                                      text: "8            ",
+                                      style: TextStyle(
+                                          fontSize: 50,
+                                          color: Colors.black
+                                      )
+                                  ),
+                                  TextSpan(
+                                      text: "9",
+                                      style: TextStyle(
+                                          fontSize: 50,
+                                          color: Colors.black
+                                      )
+                                  ),
+                                ]
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10,),
+                        Container(
+                          height: 100,
+                          width: 400,
+                          child: RichText(
+                            text: TextSpan(
+                              children: [
+                                TextSpan(
+                                  text: "  4          ",
+                                  style: TextStyle(
+                                    fontSize: 50,
+                                    color: Colors.black
+                                  )
+                                ),
+                                TextSpan(
+                                  text: "5            ",
+                                  style: TextStyle(
+                                    fontSize: 50,
+                                    color: Colors.black
+                                  )
+                                ),
+                                TextSpan(
+                                  text: "6",
+                                  style: TextStyle(
+                                    fontSize: 50,
+                                    color: Colors.black
+                                  )
+                                ),
+                              ]
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10,),
+                        Container(
+                          height: 100,
+                          width: 400,
+                          child: RichText(
+                            text: TextSpan(
+                                children: [
+                                  TextSpan(
+                                      text: "  1          ",
+                                      style: TextStyle(
+                                          fontSize: 50,
+                                          color: Colors.black
+                                      )
+                                  ),
+                                  TextSpan(
+                                      text: "2            ",
+                                      style: TextStyle(
+                                          fontSize: 50,
+                                          color: Colors.black
+                                      )
+                                  ),
+                                  TextSpan(
+                                      text: "3",
+                                      style: TextStyle(
+                                          fontSize: 50,
+                                          color: Colors.black
+                                      )
+                                  ),
+                                ]
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10,),
+                        Container(
+                          height: 100,
+                          width: 400,
+                          child: RichText(
+                            text: TextSpan(
+                                children: [
+                                  TextSpan(
+                                      text: "  00          ",
+                                      style: TextStyle(
+                                          fontSize: 50,
+                                          color: Colors.black
+                                      )
+                                  ),
+                                  TextSpan(
+                                      text: "0            ",
+                                      style: TextStyle(
+                                          fontSize: 50,
+                                          color: Colors.black
+                                      )
+                                  ),
+                                  TextSpan(
+                                      text: ".",
+                                      style: TextStyle(
+                                          fontSize: 50,
+                                          color: Colors.black
+                                      )
+                                  ),
+                                ]
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(left: 17.5),
+                        height: 229.5,
+                        width: 120,
+                        decoration: BoxDecoration(
+                          color: Colors.deepOrange,
+                          borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(65),
+                            topLeft: Radius.circular(65),
+                            bottomLeft: Radius.circular(65),
+                            bottomRight: Radius.circular(65)
+                          )
+                        ),
+                      ),
+                      SizedBox(height: 20,),
+                      Container(
+                        margin: EdgeInsets.only(left: 17.5),
+                        height: 229.5,
+                        width: 120,
+                        decoration: BoxDecoration(
+                            color: Colors.deepOrange,
+                            borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(65),
+                                topLeft: Radius.circular(65),
+                                bottomLeft: Radius.circular(65),
+                                bottomRight: Radius.circular(65)
+                            )
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
